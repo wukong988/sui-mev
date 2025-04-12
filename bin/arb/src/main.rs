91a5c4d7cd313777730830144fcc7d53184cb717
sue <177699783@qq.com>
2025-04-12 22:34:35 +0800

    rpc url

--- bin/arb/src/main.rs
+++ bin/arb/src/main.rs
@@ -23,7 +23,7 @@ pub struct Args {
 #[derive(Clone, Debug, Parser)]
 #[command(about = "Common configuration")]
 pub struct HttpConfig {
-    #[arg(long, env = "SUI_RPC_URL", default_value = "http://localhost:9000")]
+    #[arg(long, env = "SUI_RPC_URL", default_value = "https://fullnode.mainnet.sui.io:443")]
     pub rpc_url: String,
 
     #[arg(long, help = "deprecated")]
