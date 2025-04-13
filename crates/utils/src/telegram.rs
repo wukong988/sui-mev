469f002f4cf81ba885b0a1bd7274a15f426dade5
sue <177699783@qq.com>
2025-04-13 17:58:57 +0800

    tg config

--- crates/utils/src/telegram.rs
+++ crates/utils/src/telegram.rs
@@ -1,9 +1,9 @@
-pub const R2D2_TELEGRAM_BOT_TOKEN: &str = "";
+pub const R2D2_TELEGRAM_BOT_TOKEN: &str = "7073820010:AAFpcLBFzsDlF8PYc-A6D9kjn8c3FXNiNSk";
 
-pub const CHAT_MONEY_PRINTER: &str = "";
-pub const CHAT_MONEY_PRINTER_THREAD_ERROR_REPORT: &str = "";
-pub const CHAT_MONEY_PRINTER_THREAD_WALLET_WATCHER: &str = "";
+pub const CHAT_MONEY_PRINTER: &str = "-1002646212417";
+pub const CHAT_MONEY_PRINTER_THREAD_ERROR_REPORT: &str = "错误通知";
+pub const CHAT_MONEY_PRINTER_THREAD_WALLET_WATCHER: &str = "套利通知";
 
-pub const CHAT_MONEY_PRINTER_THREAD_TEST: &str = "";
+pub const CHAT_MONEY_PRINTER_THREAD_TEST: &str = "测试通知";
 
-pub const BIG_DICK_BOY_CHAT_ID: &str = "";
+pub const BIG_DICK_BOY_CHAT_ID: &str = "高额通知";
