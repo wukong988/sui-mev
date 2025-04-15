e0d1dc52ca437a6dabf8259b58beb3c0cb566e91
sue <177699783@qq.com>
2025-04-15 13:21:42 +0800

    本地节点

--- bin/arb/src/main.rs
+++ bin/arb/src/main.rs
@@ -23,7 +23,7 @@ pub struct Args {
 #[derive(Clone, Debug, Parser)]
 #[command(about = "Common configuration")]
 pub struct HttpConfig {
-    #[arg(long, env = "SUI_RPC_URL", default_value = "https://fullnode.mainnet.sui.io:443")]
+    #[arg(long, env = "SUI_RPC_URL", default_value = "http://127.0.0.1:9000")]
     pub rpc_url: String,
 
     #[arg(long, help = "deprecated")]
